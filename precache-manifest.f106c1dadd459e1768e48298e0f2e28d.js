2db7721d258dd5f5f63924d7dac6c76083d3335d
evstarostin89 <evstarostin89@yandex-team.ru>
2020-09-20 16:00:23 +0300

    Updates

---
+++ precache-manifest.f106c1dadd459e1768e48298e0f2e28d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7ec88676620926107450c27770c115b",
+    "url": "/random-video-chat/index.html"
+  },
+  {
+    "revision": "642c8db67c2e2c8bf603",
+    "url": "/random-video-chat/static/css/main.101fda9d.chunk.css"
+  },
+  {
+    "revision": "0ebf14eafb96847c1eed",
+    "url": "/random-video-chat/static/js/2.82a59d6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-video-chat/static/js/2.82a59d6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "642c8db67c2e2c8bf603",
+    "url": "/random-video-chat/static/js/main.5547fc1c.chunk.js"
+  },
+  {
+    "revision": "6f451f10c965c734a68e",
+    "url": "/random-video-chat/static/js/runtime-main.b410959e.js"
+  }
+]);
