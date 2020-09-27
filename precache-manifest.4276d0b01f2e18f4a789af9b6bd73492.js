f099396c17c4422a701e505dc3baa833d7f5b193
evstarostin89 <evstarostin89@yandex-team.ru>
2020-09-27 23:01:45 +0300

    Updates

--- precache-manifest.4276d0b01f2e18f4a789af9b6bd73492.js
+++ precache-manifest.4276d0b01f2e18f4a789af9b6bd73492.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9d550bafd9908427c6eb18c9f2cc854a",
+    "revision": "692d661c484e7317e6336459fd459243",
     "url": "/index.html"
   },
   {
-    "revision": "57ded31316681141c195",
+    "revision": "0cb739f51e59c176e3fb",
     "url": "/static/css/main.dac03b6c.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.d9e269fb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "57ded31316681141c195",
-    "url": "/static/js/main.c98f13d8.chunk.js"
+    "revision": "0cb739f51e59c176e3fb",
+    "url": "/static/js/main.6dcf11bf.chunk.js"
   },
   {
     "revision": "a75bcc7008cb4c297218",
